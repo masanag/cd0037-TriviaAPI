41f76a86876fad5a98059d1d443944a30b49d3df
masanag <masaki.nagaishi@gmail.com>
2023-09-28 19:53:09 +0900

    chore: Update TODO comments to DONE

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -22,12 +22,12 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    @DONE: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
     CORS(app, resources={r'/*': {"origins": "*"}})
 
     """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
+    @DONE: Use the after_request decorator to set Access-Control-Allow
     """
     @app.after_request
     def after_request(response):
@@ -36,7 +36,7 @@ def create_app(test_config=None):
         return response
 
     """
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests
     for all available categories.
     """
@@ -50,7 +50,7 @@ def create_app(test_config=None):
         })
 
     """
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
@@ -70,7 +70,6 @@ def create_app(test_config=None):
         if(len(current_questions) == 0):
             abort(404)
 
-        # TODO: return current category, but not sure what that means
         return jsonify({
             'success': True,
             'questions': current_questions,
@@ -78,7 +77,7 @@ def create_app(test_config=None):
             'categories': categories,
         })
     """
-    @TODO:
+    @DONE:
     Create a GET endpoint to get questions based on category.
 
     TEST: In the "List" tab / main screen, clicking on one of the
@@ -107,7 +106,7 @@ def create_app(test_config=None):
         })
 
     """
-    @TODO:
+    @DONE:
     Create an endpoint to DELETE question using a question ID.
 
     TEST: When you click the trash icon next to a question, the question will be removed.
@@ -130,7 +129,7 @@ def create_app(test_config=None):
             abort(422)
 
     """
-    @TODO:
+    @DONE:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
     category, and difficulty score.
@@ -140,8 +139,6 @@ def create_app(test_config=None):
     of the questions list in the "List" tab.
     """
     @app.route('/questions', methods=['POST'])
-    # TODO: Create POST endpoint to get questions based on category
-    # But a GET endpoint has been created with the same functionality at /categories/<int:category_id>/questions.
     def post_question():
         body = request.get_json()
         search = body.get('searchTerm', None)
@@ -153,7 +150,7 @@ def create_app(test_config=None):
 
 
     """
-    @TODO:
+    @DONE:
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
     is a substring of the question.
@@ -190,7 +187,7 @@ def create_app(test_config=None):
             abort(422)
 
     """
-    @TODO:
+    @DONE:
     Create a POST endpoint to get questions to play the quiz.
     This endpoint should take category and previous question parameters
     and return a random questions within the given category,
@@ -225,7 +222,7 @@ def create_app(test_config=None):
 
 
     """
-    @TODO:
+    @DONE:
     Create error handlers for all expected errors
     including 404 and 422.
     """
